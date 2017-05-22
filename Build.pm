c624d848e322e9a09b475bd6c8b7191ccba7fb97
titsuki <titsuki@cpan.org>
2017-05-22 23:18:56 +0900

    Improve portability (ugexe++)

--- Build.pm
+++ Build.pm
@@ -1,4 +1,7 @@
-class Builder {
+use Zef::Fetch;
+use Zef::Extract;
+
+class Build {
     method build($workdir) {
         if $*DISTRO.is-win {
             die "Sorry, this binding doesn't support windows";
@@ -7,28 +10,32 @@ class Builder {
         my $prefix = "$HOME/.p6chart-gnuplot";
         self!install-gnuplot($workdir, $prefix);
     }
-
+    
     method !install-gnuplot($workdir, $prefix) {
         my $goback = $*CWD;
         chdir($workdir);
-        my $wget-exit-code = run 'which', 'wget';
-        if $wget-exit-code == 0 {
-            my $CMD = qq:x/which wget/.subst(/\s*/,"",:g);
-	    my $url = "http://ftp.cstug.cz/pub/CTAN/graphics/gnuplot/5.0.5/gnuplot-5.0.5.tar.gz";
-	    my $dl-exit-code = run $CMD, $url, "-O", "gnuplot-5.0.5.tar.gz";
-            if not $dl-exit-code == 0 {
-                die;
-            }
-        }
-        if "gnuplot-5.0.5".IO.d {
-            run 'rm', '-rf', 'gnuplot-5.0.5';
-        }
-        run 'tar', 'xvzf', 'gnuplot-5.0.5.tar.gz';
         
-        chdir("gnuplot-5.0.5");
-        shell("./configure --prefix=$prefix");
-        shell("make");
-        shell("make install");
-        chdir($goback);
+        my @fetch-backends = [
+            { module => "Zef::Service::Shell::wget" },
+            { module => "Zef::Service::Shell::curl" },
+        ];
+        my $fetcher      = Zef::Fetch.new(:backends(@fetch-backends));
+        my $uri          = 'http://ftp.cstug.cz/pub/CTAN/graphics/gnuplot/5.0.5/gnuplot-5.0.5.tar.gz';
+        my $archive-file = $uri.IO.basename.IO.e
+        ?? $uri.IO.basename.IO
+        !! $fetcher.fetch($uri, $uri.IO.basename);
+        
+        my @extract-backends = [
+            { module => "Zef::Service::Shell::tar" },
+            { module => "Zef::Service::Shell::p5tar" },
+        ];
+        my $extractor   = Zef::Extract.new(:backends(@extract-backends));
+        my $extract-dir = $archive-file.IO.basename.subst(/\.tar\.gz/, '').IO.e
+        ?? $archive-file.IO.basename.subst(/\.tar\.gz/, '').IO
+        !! $extractor.extract($archive-file, $*CWD);
+        
+        shell("./configure --prefix=$prefix", :cwd($extract-dir.relative));
+        shell("make", :cwd($extract-dir.relative));
+        shell("make install", :cwd($extract-dir.relative));
     }
 }
