e8ded2f0f1739b9a49646443bd2dc966412b97c7
Itsuki Toyota <titsuki@cpan.org>
2017-10-28 20:34:40 +0900

    Fix extracted file dir

--- Build.pm
+++ Build.pm
@@ -1,7 +1,53 @@
+use Zef;
 use Zef::Fetch;
-use Zef::Extract;
 
 class Build {
+    # The portions of the code are licensed under the Artistic License 2.0:
+    # zef ( https://github.com/ugexe/zef ) by ugexe
+
+    # Relicensed under the GPL:
+    # p6-ChartGnuplot by titsuki
+
+    method extract(IO() $archive-file, IO() $extract-to) {
+        die "archive file does not exist: {$archive-file.absolute}"
+        unless $archive-file.e && $archive-file.f;
+        die "target extraction directory {$extract-to.absolute} does not exist and could not be created"
+        unless ($extract-to.e && $extract-to.d) || mkdir($extract-to);
+
+        my $passed;
+        react {
+            my $cwd := $archive-file.parent;
+            my $ENV := %*ENV;
+            my $proc = zrun-async('tar', '-zxvf', $archive-file.basename, '-C', $extract-to.relative($cwd));
+            whenever $proc.stdout(:bin) { }
+            whenever $proc.stderr(:bin) { }
+            whenever $proc.start(:$ENV, :$cwd) { $passed = $_.so }
+        }
+
+        my $meta6-prefix = self.list($archive-file).sort.first({ .IO.basename eq 'VERSION' });
+        my $extracted-to = $extract-to.child($meta6-prefix);
+        ($passed && $extracted-to.e) ?? $extracted-to.parent !! False;
+    }
+
+    method list(IO() $archive-file) {
+        die "archive file does not exist: {$archive-file.absolute}"
+        unless $archive-file.e && $archive-file.f;
+
+        my $passed;
+        my $output = Buf.new;
+        react {
+            my $cwd := $archive-file.parent;
+            my $ENV := %*ENV;
+            my $proc = zrun-async('tar', '--list', '-f', $archive-file.basename);
+            whenever $proc.stdout(:bin) { $output.append($_) }
+            whenever $proc.stderr(:bin) { }
+            whenever $proc.start(:$ENV, :$cwd) { $passed = $_.so }
+        }
+
+        my @extracted-paths = $output.decode.lines;
+        $passed ?? @extracted-paths.grep(*.defined) !! ();
+    }
+
     method build($workdir) {
         if $*DISTRO.is-win {
             die "Sorry, this binding doesn't support windows";
@@ -10,7 +56,7 @@ class Build {
         my $prefix = "$HOME/.p6chart-gnuplot";
         self!install-gnuplot($workdir, $prefix);
     }
-    
+
     method !install-gnuplot($workdir, $prefix) {
         my $goback = $*CWD;
         chdir($workdir);
@@ -25,15 +71,10 @@ class Build {
         ?? $uri.IO.basename.IO
         !! $fetcher.fetch($uri, $uri.IO.basename);
         
-        my @extract-backends = [
-            { module => "Zef::Service::Shell::tar" },
-            { module => "Zef::Service::Shell::p5tar" },
-        ];
-        my $extractor   = Zef::Extract.new(:backends(@extract-backends));
         my $extract-dir = $archive-file.IO.basename.subst(/\.tar\.gz/, '').IO.e
         ?? $archive-file.IO.basename.subst(/\.tar\.gz/, '').IO
-        !! $extractor.extract($archive-file, $*CWD);
-        
+        !! self.extract($archive-file, $*CWD);
+
         shell("./configure --prefix=$prefix", :cwd($extract-dir.relative));
         shell("make", :cwd($extract-dir.relative));
         shell("make install", :cwd($extract-dir.relative));
